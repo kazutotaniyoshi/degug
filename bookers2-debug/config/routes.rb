012a80d2a132d82de4b6765891e9b86ff49e31bf
EC2 Default User <it352032@gmail.com>
2021-11-02 13:08:57 +0000

    応用２完了

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   devise_for :users
+  resources :books
+  resources :users,only: [:show,:index,:edit,:update]
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 end
