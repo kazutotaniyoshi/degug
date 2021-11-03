6a319f947209e789ce3646bfe33279d100751d5e
EC2 Default User <it352032@gmail.com>
2021-11-03 09:25:59 +0000

    test

--- bookers2-debug/app/controllers/favorites_controller.rb
+++ bookers2-debug/app/controllers/favorites_controller.rb
@@ -1,16 +1,21 @@
 class FavoritesController < ApplicationController
-  def create
+    def create
 
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to books_path
-  end
+      book = Book.find(params[:book_id])
+      favorite = current_user.favorites.new(book_id: book.id)
+      favorite.save
+      redirect_to books_path
+    end
 
-  def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-      favorite.destroy
-      redirect_to book_path
-  end
+    def destroy
+      book = Book.find(params[:book_id])
+      favorite = current_user.favorites.find_by(book_id: book.id)
+        favorite.destroy
+        redirect_to book_path
+    end
+
+    def show
+      @book = Book.find(params[:id])
+      @post_comment = PostComment.new
+    end
 end
--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books do
     resource :favorites, only: [:create, :destroy]
+    
   end
   resources :users,only: [:show,:index,:edit,:update]
   root to: 'homes#top'
