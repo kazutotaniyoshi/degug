a85f6f5266facb95a915cec1a9fd809d191deba6
EC2 Default User <it352032@gmail.com>
2021-11-03 12:29:07 +0000

    応用課題３完了

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books do
     resource :favorites, only: [:create, :destroy]
-    
+    resources :post_comments, only: [:create, :destroy]
+
   end
   resources :users,only: [:show,:index,:edit,:update]
   root to: 'homes#top'
--- bookers2-debug/db/schema.rb
+++ bookers2-debug/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_091538) do
+ActiveRecord::Schema.define(version: 2021_11_03_113345) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_11_03_091538) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "book_id"
   end
 
   create_table "post_comments", force: :cascade do |t|
@@ -31,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_11_03_091538) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
