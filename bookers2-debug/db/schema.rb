b8c0dd93aa192ea8fd8ab74ed6d4975c46a83c09
EC2 Default User <it352032@gmail.com>
2021-11-04 04:51:07 +0000

    t

--- bookers2-debug/db/schema.rb
+++ bookers2-debug/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_113345) do
+ActiveRecord::Schema.define(version: 2021_11_04_023326) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_11_03_113345) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "book_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
